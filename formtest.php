7057bfb6b35583265cc79111b8e420ab92c5f7f5
Your Name <you@example.com>
2017-11-12 20:57:17 +0100

    tweede commit

--- formtest.php
+++ formtest.php
@@ -1,16 +1,32 @@
-<?php // formtest.php
-    echo <<<_END
-    <html>
-        <head>
-            <title>Form Test</title>
-        </head>
-        <body>
-            <form method="post" action="formtest.php">
-                What is your name?
-                    <input type="text" name="name">
-                    <input type="submit">
-            </form>
-        </body>
-    </html>
-_END;
-?>
+<html>
+    <head>
+     
+        <title>Form</title>
+   
+    </head>
+    <body>
+        <p> <h2>Registrate</h2>
+        <table> 
+            <tr>
+                <td  
+                    <p><h4>Your Info</h4></p>
+                    <form action="action_page.php" method="GET">
+                        
+                        First name: <input type="text" name="FirstName" value="" required><br>
+                        Last name: <input type="text" name="LastName" value="" required><br><br>
+                       <input type="radio" name="Gender" value="Female">Male<br>
+                        <input type="radio" name="Gender" value="Male">Female<br><br> 
+                        Date of birth<input type="text" name="DateOfBirth" value="" maxlength="10" size="10">(dd-mm-yyyy)<br><br>
+                        <p><h4>Address</h4></p>
+                        Street  <input type="text" name="Street" value="" required><br>
+                        Zipcode <input type="text" name="Zipcode" value=""><br>
+                        City    <input type="text" name="City" value="" required><br><br>
+                    
+                        E-mail: <input type="email" name="usermail" required>                                      
+                        <input type="submit" value="Submit"><br>       
+                    </form>
+                </td>
+            </tr>  
+    </body>
+    </center>
+</html>
